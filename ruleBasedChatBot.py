aacbb00d2b56193820741538bbaa37ca38775dd7
christopher827 <christopheroche827@gmail.com>
2024-03-05 04:23:07 +0100

    first commit

---
+++ ruleBasedChatBot.py
@@ -0,0 +1,75 @@
+from nltk.chat.util import Chat, reflections
+#creates a list of recognizable patterns and different responses to those patterns
+pairs = [
+    [
+        r"(.*)my name is (.*)",
+        ["Hello %2, How are you today ?",]
+    ],
+    [
+        r"(.*)help(.*) ",
+        ["I can help you ",]
+    ],
+     [
+        r"(.*) your name ?",
+        ["My name is chrisTheScientist, but you can just call me robot and I'm a chatbot .",]
+    ],
+    [
+        r"how are you (.*) ?",
+        ["I'm doing very well", "i am great !"]
+    ],
+    [
+        r"sorry (.*)",
+        ["Its alright","Its OK, never mind that",]
+    ],
+    [
+        r"i'm (.*) (good|well|okay|ok)",
+        ["Nice to hear that","Alright, great !",]
+    ],
+    [
+        r"(hi|hey|hello|hola|holla)(.*)",
+        ["Hello", "Hey there",]
+    ],
+    [
+        r"what (.*) want ?",
+        ["Make me an offer I can't refuse",]
+        
+    ],
+    [
+        r"(.*)created(.*)",
+        ["Christopher Oche created me using Python's NLTK library ","top secret ;)",]
+    ],
+    [
+        r"(.*) (location|city) ?",
+        ['Lagos, Nigeria',]
+    ],
+    [
+        r"(.*)raining in (.*)",
+        ["No rain in the past 4 days here in %2","In %2 there is a 50% chance of rain",]
+    ],
+    [
+        r"how (.*) health (.*)",
+        ["Health is very important, but I am a computer, so I don't need to worry about my health ",]
+    ],
+    [
+        r"(.*)(sports|game|sport)(.*)",
+        ["I'm a very big fan of Cricket",]
+    ],
+    [
+        r"who (.*) (Cricketer|Batsman)?",
+        ["Virat Kohli"]
+    ],
+    [
+        r"quit",
+        ["Bye for now. See you soon :) ","It was nice talking to you. See you soon :)"]
+    ],
+    [
+        r"(.*)", #if none of the previous prompts was entered/used while chatting
+        ['That is nice to hear']
+    ],
+]
+print(reflections)
+print("Hi, I'm Ossy Gram and I like to chat\nPlease type lowercase English language to start a conversation. Type quit to leave ")
+#Create Chat Bot
+chat = Chat(pairs, reflections)
+#Start conversation
+chat.converse()
